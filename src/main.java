006daa7d26aafcafca7552655f142924d832cf73
Hye Jin <minh@ufl.edu>
2017-09-26 12:14:26 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scnr = new Scanner(System.in);
+
+        int smallCode = 0;
+
+        smallCode = scnr.nextInt();
+        System.out.println("Output this message: " + smallCode);
+    }
+}
+
